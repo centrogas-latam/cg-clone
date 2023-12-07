9d80260f6290aa7ae3279e4c4bb7c7141e0e8398
Manutebi <ma.aninir@duocuc.cl>
2023-12-07 18:17:46 -0300

    Responsive, menu,

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,27 @@
 import ButtonSubmmit from "@/components/ButtonSubmmit";
 import { Button } from "@/components/ui/button";
-import {
-  Award,
-  Cpu,
-  Facebook,
-  Instagram,
-  Linkedin,
-  PhoneCall,
-  Wrench,
-} from "lucide-react";
+import { Award, Cpu, Instagram, PhoneCall, Wrench } from "lucide-react";
 import Image from "next/image";
 import { formSubmit } from "./action";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col px-36">
-        <div className="flex mt-36">
-          <div className="pr-16">
-            <h1 className="text-5xl font-semibold">
+      <div className="flex flex-col px-4 md:px-36">
+        <div className="flex flex-col md:flex-row mt-8 md:mt-36">
+          <div className="pr-0 md:pr-16">
+            <h1 className="text-2xl md:text-5xl font-semibold">
               <span className="text-[#00ad10]">Especialistas</span> en manteción
               a <span className="text-[#2245e3]">comunidades</span>
             </h1>
-            <p className="my-10">
+            <p className="my-4 md:my-10">
               ¡Bienvenidos a CentroGas, tu aliado confiable en el mundo de la
               eficiencia energética y el mantenimiento de edificios! En un
               panorama donde la calidad y el rendimiento son fundamentales, nos
               enorgullece presentarnos como líderes en el cuidado integral de tu
               comunidad.
             </p>
-            <div className="flex gap-2">
+            <div className="flex gap-2 mb-7">
               <Button className="bg-[#2245e3]">Descargar Brochure</Button>
               <Button className="border-[#00ad10]" variant="outline">
                 Contratar
@@ -44,17 +36,17 @@ export default function Home() {
           />
         </div>
 
-        <div className="my-44 ">
-          <h1 className="text-3xl font-semibold flex gap-1 w-full justify-center mb-8">
+        <div id="quienes-somos" className="my-8 md:my-44">
+          <h1 className="text-xl md:text-3xl font-semibold flex gap-1 w-full justify-center mb-4 md:mb-8">
             Quienes <span className="text-[#2245e3]">somos</span>
           </h1>
-          <h2 className="text-2xl font-semibold mb-2">
+          <h2 className="text-xl md:text-2xl font-semibold mb-2">
             <span className="text-[#2245e3]">centro</span>
             <span className="text-[#00ad10]">gas</span>
           </h2>
-          <div className="flex">
+          <div className="flex flex-col md:flex-row">
             <div>
-              <p className="pr-8 mb-4">
+              <p className="pr-4 md:pr-8 mb-4">
                 En CentroGas, nos especializamos en la mantención de edificios,
                 brindando soluciones completas para garantizar el óptimo
                 funcionamiento de cada componente crucial. Desde salas de
@@ -62,9 +54,9 @@ export default function Home() {
                 y paneles solares, abordamos cada detalle con experiencia y
                 dedicación.
               </p>
-              <div className="flex pr-8 text-justify gap-8 mt-12">
-                <div className="w-1/2">
-                  <h2 className="text-xl font-medium text-[#2245e3] mb-2">
+              <div className="flex flex-col md:flex-row pr-4 md:pr-8 text-justify gap-8 mt-6 md:mt-12">
+                <div className="w-full md:w-1/2">
+                  <h2 className="text-lg md:text-xl font-medium text-[#2245e3] mb-2">
                     ¿Quiénes somos?
                   </h2>
                   <p>
@@ -79,11 +71,11 @@ export default function Home() {
                     expectativas.
                   </p>
                 </div>
-                <div className="w-1/2">
-                  <h2 className="text-xl font-medium text-[#00ad10] mb-2">
+                <div className="w-full md:w-1/2">
+                  <h2 className="text-lg md:text-xl font-medium text-[#00ad10] mb-2">
                     ¿Porqué elegirnos?
                   </h2>
-                  <ul className="list-disc ml-5">
+                  <ul className="list-disc ml-5 mb-4">
                     <li>
                       Contamos con un equipo profesional y especialista
                       certificado ante la Superintendencia de Electricidad y
@@ -110,15 +102,16 @@ export default function Home() {
           </div>
         </div>
 
-        <div>
+        <div id="soluciones">
           <h1 className="text-3xl font-semibold flex gap-1 w-full justify-center mb-24">
             Nuestras <span className="text-[#00ad10]">soluciones</span>
           </h1>
-
           <div className="flex flex-col">
-            <div className="flex gap-36">
+            <div className="flex flex-col md:flex-row gap-4 md:gap-36">
               <div className="self-center">
-                <h1 className="text-2xl font-semibold">Salas térmicas</h1>
+                <h1 className="text-xl md:text-2xl font-semibold">
+                  Salas térmicas
+                </h1>
                 <p className="text-justify my-4">
                   Nos especializamos en el mantenimiento de salas de calderas,
                   asegurando un rendimiento óptimo y la máxima eficiencia
@@ -127,7 +120,7 @@ export default function Home() {
                   fallas. Trabajamos para garantizar un ambiente seguro y
                   cumplir con los estándares de calidad.
                 </p>
-                <Button className="bg-[#2245e3]">Contizar</Button>
+                <Button className="bg-[#2245e3]">Cotizar</Button>
               </div>
               <Image
                 src="https://eihymsfhikncmipfrakl.supabase.co/storage/v1/object/public/mp3/IMG_8061.jpg"
@@ -136,7 +129,8 @@ export default function Home() {
                 alt="Sala termica"
               />
             </div>
-            <div className="flex gap-36 mt-24">
+
+            <div className="flex flex-col md:flex-row gap-4 md:gap-36 mt-8 md:mt-24">
               <Image
                 src="https://eihymsfhikncmipfrakl.supabase.co/storage/v1/object/public/mp3/IMG_7486%20(1).jpg"
                 width={500}
@@ -144,7 +138,9 @@ export default function Home() {
                 alt="Sala termica"
               />
               <div className="self-center">
-                <h1 className="text-2xl font-semibold">Sala de bombas</h1>
+                <h1 className="text-xl md:text-2xl font-semibold">
+                  Sala de bombas
+                </h1>
                 <p className="text-justify my-4">
                   Proporcionamos servicios de mantenimiento para salas de
                   bombas, asegurando un flujo constante y eficiente de agua.
@@ -153,12 +149,15 @@ export default function Home() {
                   rendimiento óptimo, contribuyendo así a la eficiencia
                   operativa y ahorro de energía.
                 </p>
-                <Button className="bg-[#00ad10]">Contizar</Button>
+                <Button className="bg-[#00ad10]">Cotizar</Button>
               </div>
             </div>
-            <div className="flex gap-36 mt-24">
+
+            <div className="flex flex-col md:flex-row gap-4 md:gap-36 mt-8 md:mt-24">
               <div className="self-center">
-                <h1 className="text-2xl font-semibold">Grupo electrógeno</h1>
+                <h1 className="text-xl md:text-2xl font-semibold">
+                  Grupo electrógeno
+                </h1>
                 <p className="text-justify my-4">
                   Nos ocupamos del mantenimiento preventivo y correctivo de
                   grupos electrógenos, asegurando la disponibilidad de energía
@@ -167,7 +166,7 @@ export default function Home() {
                   esté listo para entrar en acción en caso de cortes de energía,
                   brindando tranquilidad a nuestros clientes.
                 </p>
-                <Button className="bg-[#2245e3]">Contizar</Button>
+                <Button className="bg-[#2245e3]">Cotizar</Button>
               </div>
               <Image
                 src="https://eihymsfhikncmipfrakl.supabase.co/storage/v1/object/public/mp3/generador-electrico-diferencias.webp"
@@ -176,7 +175,8 @@ export default function Home() {
                 alt="Sala termica"
               />
             </div>
-            <div className="flex gap-36 mt-24">
+
+            <div className="flex flex-col md:flex-row gap-4 md:gap-36 mt-8 md:mt-24">
               <Image
                 src="https://eihymsfhikncmipfrakl.supabase.co/storage/v1/object/public/mp3/granja-alimentada-sol-genera-electricidad-sostenible-naturaleza-generada-ia.jpg"
                 width={500}
@@ -184,7 +184,9 @@ export default function Home() {
                 alt="Sala termica"
               />
               <div className="self-center">
-                <h1 className="text-2xl font-semibold">Paneles Solares</h1>
+                <h1 className="text-xl md:text-2xl font-semibold">
+                  Paneles Solares
+                </h1>
                 <p className="text-justify my-4">
                   Ofrecemos servicios especializados en el mantenimiento de
                   sistemas de paneles solares para garantizar su máximo
@@ -193,12 +195,14 @@ export default function Home() {
                   genere la energía esperada y contribuya a la sostenibilidad
                   ambiental de manera óptima.
                 </p>
-                <Button className="bg-[#00ad10]">Contizar</Button>
+                <Button className="bg-[#00ad10]">Cotizar</Button>
               </div>
             </div>
-            <div className="flex gap-36 mt-24">
+            <div className="flex flex-col md:flex-row gap-4 md:gap-36 mt-8 md:mt-24">
               <div className="self-center">
-                <h1 className="text-2xl font-semibold">Aguas Servidas</h1>
+                <h1 className="text-xl md:text-2xl font-semibold">
+                  Aguas Servidas
+                </h1>
                 <p className="text-justify my-4">
                   Nos encargamos de la reparación de posibles averías y la
                   implementación de medidas preventivas para asegurar el
@@ -219,8 +223,8 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mt-40 flex">
-          <div className="w-1/3">
+        <div className="mt-8 md:mt-40 flex flex-col md:flex-row">
+          <div className="w-full md:w-1/3">
             <Image
               src="https://eihymsfhikncmipfrakl.supabase.co/storage/v1/object/public/mp3/hombre-dando-presentacion-grafico-barras-lapiz-digital-alta-tecnologia.jpg"
               width={550}
@@ -228,15 +232,15 @@ export default function Home() {
               alt="Hombre gráfico"
             />
           </div>
-          <div className="w-2/3 ml-8">
-            <h1 className="text-2xl font-semibold text-[#2245e3] mb-3">
+          <div className="w-full md:w-2/3 ml-0 md:ml-8">
+            <h1 className="text-xl md:text-2xl font-semibold py-10 text-[#2245e3] mb-3">
               Beneficios Clave
             </h1>
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               {/* Tarjeta 1: Experiencia y Profesionalismo */}
               <div className="p-4 border rounded-lg text-center">
                 <Award className="mb-2 mx-auto text-[#00ad10]" />
-                <h1 className="text-xl font-semibold mb-2">
+                <h1 className="text-lg md:text-xl font-semibold mb-2">
                   Experiencia y Profesionalismo
                 </h1>
                 <p>
@@ -244,11 +248,10 @@ export default function Home() {
                   efectivas.
                 </p>
               </div>
-
               {/* Tarjeta 2: Tecnología Innovadora */}
               <div className="p-4 border rounded-lg text-center">
                 <Cpu className="mb-2 mx-auto text-[#2245e3]" />
-                <h1 className="text-xl font-semibold mb-2">
+                <h1 className="text-lg md:text-xl font-semibold mb-2">
                   Tecnología Innovadora
                 </h1>
                 <p>
@@ -256,11 +259,10 @@ export default function Home() {
                   eficientes.
                 </p>
               </div>
-
               {/* Tarjeta 3: Servicio Integral */}
               <div className="p-4 border rounded-lg text-center">
                 <Wrench className="mb-2 mx-auto text-[#2245e3]" />
-                <h1 className="text-xl font-semibold mb-2">
+                <h1 className="text-lg md:text-xl font-semibold mb-2">
                   Servicio Integral
                 </h1>
                 <p>
@@ -272,7 +274,7 @@ export default function Home() {
               {/* Tarjeta 4: Disponibilidad y Respuesta Rápida */}
               <div className="p-4 border rounded-lg text-center">
                 <PhoneCall className="mb-2 mx-auto text-[#00ad10]" />
-                <h1 className="text-xl font-semibold mb-2">
+                <h1 className="text-lg md:text-xl font-semibold mb-2">
                   Disponibilidad y Respuesta Rápida
                 </h1>
                 <p>
@@ -284,7 +286,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex mt-40 gap-8">
+        <div id="contacto" className="md:flex mt-40 gap-8">
           {/* Sección del Formulario */}
           <div className="w-full md:w-1/2">
             <h1 className="text-2xl font-semibold mb-5">Contáctanos</h1>
@@ -333,7 +335,7 @@ export default function Home() {
           </div>
 
           {/* Sección de la Imagen */}
-          <div className="w-full md:w-1/2">
+          <div className="w-full md:w-1/2 mt-10">
             <Image
               src="https://eihymsfhikncmipfrakl.supabase.co/storage/v1/object/public/mp3/persona-escribiendo-computadora-portatil-noche-generada-ia.jpg"
               alt="Persona escribiendo en computadora portátil"
@@ -345,20 +347,19 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className="mt-20 h-64 bg-[#2245e3] w-full flex items-start justify-between text-white p-10">
+      <footer className="mt-10 md:mt-40  bg-[#2245e3] w-full flex flex-col md:flex-row items-start justify-between text-white p-10 ">
         {/* Logotipo y nombre */}
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center mb-10 md:mb-0">
           <Image
             src="/logo-blanco.svg"
             alt="Logo CentroGas"
             width={189}
             height={143}
-            className=""
           />
         </div>
 
         {/* Sección de Servicios */}
-        <div className="flex flex-col">
+        <div className="flex flex-col mb-10 md:mb-0">
           <h3 className="font-semibold mb-4">Servicios</h3>
           <ul className="space-y-2">
             <li>Sala de calderas</li>
@@ -370,7 +371,7 @@ export default function Home() {
         </div>
 
         {/* Información de Contacto */}
-        <div className="flex flex-col">
+        <div className="flex flex-col mb-10 md:mb-0">
           <h3 className="font-semibold mb-4">Contacto</h3>
           <ul className="space-y-2">
             <li>Las Condes, Santiago</li>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,37 +1,39 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
 
 let tabs = [
-  { id: "world", label: "Inicio" },
-  { id: "business", label: "Quiénes somos" },
-  { id: "ny", label: "Soluciones" },
-  { id: "arts", label: "Contacto" },
+  { id: "inicio", label: "Inicio" },
+  { id: "quienes-somos", label: "Quiénes somos" },
+  { id: "soluciones", label: "Soluciones" },
+  { id: "contacto", label: "Contacto" },
 ];
 
 const Navbar = () => {
   let [activeTab, setActiveTab] = useState(tabs[0].id);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-md transition-all px-36">
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-md transition-all px-10">
       <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-        <div className="md:hidden"></div>
-        <Link href="/" className="flex z-40 font-semibold w-full">
+        <a href="#inicio" className="flex z-40 font-semibold w-full">
           <Image
             src="centrogasvectorizado8.svg"
             alt="logo"
             width={65}
             height={65}
           />
-        </Link>
-        <div className="flex space-x-1 w-full justify-center">
+        </a>
+        <div className="hidden md:flex space-x-1 w-full justify-center">
           {tabs.map((tab) => (
-            <button
+            <a
               key={tab.id}
+              href={`#${tab.id}`}
               onClick={() => setActiveTab(tab.id)}
               className={`${
                 activeTab === tab.id ? "" : "hover:text-gray-500"
@@ -49,15 +51,49 @@ const Navbar = () => {
                 />
               )}
               {tab.label}
-            </button>
+            </a>
           ))}
         </div>
-        <div className="flex  gap-2 w-full justify-end">
+        <div className="hidden md:flex gap-2 w-full justify-end">
           <Button className="bg-[#00ad10]">Cotiza</Button>
           <Button className="bg-[#2245e3]">Contactanos</Button>
         </div>
+        <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+          <Menu color="black" size={24} />
+        </button>
       </div>
+      {isOpen && (
+        <div className="menu md:hidden">
+          {tabs.map((tab) => (
+            <a
+              key={tab.id}
+              href={`#${tab.id}`}
+              onClick={() => {
+                setActiveTab(tab.id);
+                setIsOpen(false);
+              }}
+              className={`${
+                activeTab === tab.id ? "" : "hover:text-gray-500"
+              } relative rounded-none px-3 py-1.5 text-sm font-medium text-black outline-sky-400 transition focus-visible:outline-2`}
+              style={{
+                WebkitTapHighlightColor: "transparent",
+              }}
+            >
+              {activeTab === tab.id && (
+                <motion.span
+                  layoutId="bubble"
+                  className="absolute inset-0 z-10 bg-black mix-blend-difference"
+                  style={{ borderRadius: 10 }}
+                  transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+                />
+              )}
+              {tab.label}
+            </a>
+          ))}
+        </div>
+      )}
     </nav>
   );
 };
+
 export default Navbar;
